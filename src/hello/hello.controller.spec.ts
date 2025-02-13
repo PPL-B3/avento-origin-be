7c9a83670012ef3e4294484913f1c3c2b6a31645
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-02-13 13:42:07 +0700

    add initial test and ci/cd

---
+++ src/hello/hello.controller.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HelloController } from './hello.controller';
+import { HelloService } from './hello.service';
+
+describe('HelloController', () => {
+  let controller: HelloController;
+  let helloService: HelloService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HelloController],
+      providers: [
+        {
+          provide: HelloService,
+          useValue: {
+            getHello: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<HelloController>(HelloController);
+    helloService = module.get<HelloService>(HelloService);
+  });
+
+  it('should return messages from HelloService', async () => {
+    const expected = { messages: ['Hello from controller'] };
+    (helloService.getHello as jest.Mock).mockResolvedValue(expected);
+
+    const result = await controller.getHello();
+    expect(result).toEqual(expected);
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(helloService.getHello).toHaveBeenCalled();
+  });
+});
