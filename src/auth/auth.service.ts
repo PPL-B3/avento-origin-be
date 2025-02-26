925b417ad6a85146d64e80807d21eb8779e89e92
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-02-26 21:42:42 +0700

    [GREEN] implement auth service and controller

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async register(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        password: hash,
+        role: 'user',
+      },
+      select: {
+        id: true,
+        email: true,
+      },
+    });
+    return user;
+  }
+}
