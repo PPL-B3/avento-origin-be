1300f0ca1300d7ccefe9e01b12a230864ce182ab
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-03-03 16:10:50 +0700

    feat(register): [GREEN] add basic exception handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -9,17 +10,28 @@ export class AuthService {
 
   async register(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-    const user = await this.prismaService.user.create({
-      data: {
-        email: dto.email,
-        password: hash,
-        role: 'user',
-      },
-      select: {
-        id: true,
-        email: true,
-      },
-    });
-    return user;
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+          role: 'user',
+        },
+      });
+
+      return {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      };
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Email has already been registered');
+        }
+      } else {
+        throw err;
+      }
+    }
   }
 }
