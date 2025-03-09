ea01f18f138e78fd42aabd87304839eb290679b3
BimantoroW <124970245+BimantoroW@users.noreply.github.com>
2025-03-09 18:00:49 +0700

    Revert "Feat/bryan"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { DocumentModule } from "./document/document.module";
-import { Module } from "@nestjs/common";
-import { PrismaModule } from "./prisma/prisma.module";
-import { PrismaService } from "./prisma/prisma.service";
+import { Module } from '@nestjs/common';
+import { HelloModule } from './hello/hello.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [DocumentModule, PrismaModule],
+  imports: [HelloModule, PrismaModule],
   providers: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import { ValidationPipe } from "@nestjs/common";
-import { AppModule } from "./app.module";
-import { NestFactory } from "@nestjs/core";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
