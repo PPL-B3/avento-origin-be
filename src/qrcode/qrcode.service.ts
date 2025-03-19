a147a5dd2047523ac6a57d24ff525b9b2369b955
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-03-19 20:50:03 +0700

    feat(qr): [GREEN] implement qr code generation

---
+++ src/qrcode/qrcode.service.ts
@@ -0,0 +1,50 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class QrcodeService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async generateQr(documentId: string) {
+    try {
+      await this.prismaService.document.findUniqueOrThrow({
+        where: { documentID: documentId },
+      });
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === "P2025") {
+          throw new BadRequestException("No document found with such ID");
+        }
+      }
+      throw err;
+    }
+
+    const privateQr = await this.prismaService.qRCode.create({
+      data: {
+        documentId,
+        isPrivate: true,
+        isActive: true,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const publicQr = await this.prismaService.qRCode.create({
+      data: {
+        documentId,
+        isPrivate: false,
+        isActive: true,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    return {
+      privateId: privateQr.id,
+      publicId: publicQr.id,
+    };
+  }
+}
