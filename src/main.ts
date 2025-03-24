864f9f966fb5c79a63914c9545e650b2702d9896
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-03-24 16:02:06 +0700

    feat(docs): add swagger dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
   );
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle("Avento Origin")
+    .setDescription("Avento Origin API Documentation")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
