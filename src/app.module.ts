0f9cae46fd6d66d8413db64bbe381c1811df70fc
rhaken <rhakenss@gmail.com>
2025-04-07 12:28:30 +0700

    (fix) add  module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { AuthModule } from "./auth/auth.module";
 // import { JwtAuthMiddleware } from "./auth/jwt/middleware/jwt-auth.middleware";
 import { ConfigModule } from "@nestjs/config";
 import { QrcodeModule } from "./qrcode/qrcode.module";
+import {AuditLogModule} from "./auditLog/auditLog.module";
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { QrcodeModule } from "./qrcode/qrcode.module";
       isGlobal: true,
     }),
     QrcodeModule,
+    AuditLogModule,
   ],
   providers: [PrismaService],
 })
