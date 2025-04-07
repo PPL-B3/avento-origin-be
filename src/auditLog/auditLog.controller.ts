266bc9557b16427870df3093f784f9882ea7c3f3
rhaken <rhakenss@gmail.com>
2025-04-07 11:54:28 +0700

    [GREEN] Add GetAllAuditLog controller

---
+++ src/auditLog/auditLog.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { AuditLogService } from "./auditLog.service";
+
+@Controller("audit-log")
+export class AuditLogController {
+  constructor(private readonly auditLogService: AuditLogService) {}
+
+  @Get()
+  async getAllAuditLogs() {
+    return this.auditLogService.getAllAuditLogs();
+  }
+}
