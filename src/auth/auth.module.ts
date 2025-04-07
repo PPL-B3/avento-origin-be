450b4cd21489b35b963f658b87c6bad53cda896e
rhaken <rhakenss@gmail.com>
2025-04-07 15:47:24 +0700

    [REFACTOR] integrate logging into user registration feature

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtService } from "./jwt/jwt.service";
+import {AuditLogModule} from "../auditLog/auditLog.module";
 
 @Module({
+  imports: [AuditLogModule],
   providers: [AuthService, JwtService],
   controllers: [AuthController],
   exports: [JwtService],
