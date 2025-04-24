ce534206da31f53c4735eb316edc9e8db8175bbd
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-04-24 08:19:29 +0700

    chore: fix typo in error message

--- src/document/services/document.service.ts
+++ src/document/services/document.service.ts
@@ -207,7 +207,7 @@ export class DocumentService {
       throw new NotFoundException("QR code not found");
     }
     if (!qrCode.isActive) {
-      throw new BadRequestException("QR code exits but is inactive");
+      throw new BadRequestException("QR code exists but is inactive");
     }
 
     const document = qrCode.document;
