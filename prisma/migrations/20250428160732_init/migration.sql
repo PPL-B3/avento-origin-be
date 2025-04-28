f668651c10991e88a7f1e78211945a66db921289
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-04-28 23:07:58 +0700

    chore: reset migrations

--- prisma/migrations/20250428160732_init/migration.sql
+++ prisma/migrations/20250428160732_init/migration.sql
@@ -13,6 +13,7 @@ CREATE TABLE "messages" (
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'USER',
@@ -76,6 +77,9 @@ CREATE TABLE "audit_logs" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_createdAt_idx" ON "users"("createdAt");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "qrcode_otps_qrCodeId_key" ON "qrcode_otps"("qrCodeId");
