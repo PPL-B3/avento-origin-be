72ef9134e3274cf37cb32a3ef84beb481d8f1777
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-04-29 00:19:27 +0700

    chore: reset migrations

--- prisma/migrations/20250428171901_init/migration.sql
+++ prisma/migrations/20250428171901_init/migration.sql
@@ -25,7 +25,6 @@ CREATE TABLE "Document" (
     "filePath" TEXT NOT NULL,
     "uploadDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "publisher" TEXT NOT NULL,
-    "ownerCount" INTEGER NOT NULL,
     "pendingOwner" TEXT,
     "otp" TEXT,
     "otpExpiry" TIMESTAMP(3),
@@ -41,10 +40,21 @@ CREATE TABLE "qrcodes" (
     "isPrivate" BOOLEAN NOT NULL,
     "isActive" BOOLEAN NOT NULL,
     "generatedDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ownerNumber" INTEGER NOT NULL,
     "documentId" TEXT NOT NULL,
 
     CONSTRAINT "qrcodes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "audit_logs" (
+    "logID" TEXT NOT NULL,
+    "eventType" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userID" TEXT NOT NULL,
+    "documentID" TEXT,
+    "details" TEXT NOT NULL,
+
+    CONSTRAINT "audit_logs_pkey" PRIMARY KEY ("logID")
 );
 
 -- CreateIndex
@@ -52,3 +62,6 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "qrcodes" ADD CONSTRAINT "qrcodes_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("documentID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "audit_logs" ADD CONSTRAINT "audit_logs_documentID_fkey" FOREIGN KEY ("documentID") REFERENCES "Document"("documentID") ON DELETE SET NULL ON UPDATE CASCADE;
