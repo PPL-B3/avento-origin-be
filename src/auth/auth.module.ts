5403ceabf020a0ab70ab20f7d1a9a6dd65702ada
Bimantoro <bimantoro.widyadana@ui.ac.id>
2025-05-19 22:50:28 +0700

    feat(PPL-126): [GREEN] implement bruteforce mitigation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtService } from "./jwt/jwt.service";
-import {AuditLogModule} from "../auditLog/auditLog.module";
+import { AuditLogModule } from "../auditLog/auditLog.module";
 
 @Module({
   imports: [AuditLogModule],
